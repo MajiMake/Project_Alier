91e6ffe85be7e6619916f511bace8f3524a252fc
EllyIMA <wiz33@yandex.ru>
2022-03-16 12:02:00 +0300

    test2

--- vectors.py
+++ vectors.py
@@ -1,50 +1,60 @@
-class Vector_calc:
-    def __init__(self, *args, **kwargs):
-        self.b2 = args[4]
-        self.a2 = args[1]
-        self.a1 = args[0]
-        self.b1 = args[3]
-        self.a3 = args[2]
-        self.b3 = args[5]
-        self_sum = kwargs
+def Calc(operation):
+    if operation == '+':
+        def calc(a1,a2,b1,b2,b3 = None, a3 = None):
+            c1 = a1 + b1
+            c2 = a2 + b2
+            c3 =  a3 + b3
+            return c1,c2,c3
 
-    if self.a3 is None:
-        def vector_calc(self):
-            if self.sum == '+':
-                first_sum = self.a1 + self.b1
-                second_sum = self.a2 + self.b2
-                return '({},{})'.format(first_sum, second_sum)  # добавить вторую функцию для 3х векторов
-            elif self.sum == '-':
-                first_sum = self.a1 - self.b1
-                second_sum = self.a2 - self.b2
-                return '({},{})'.format(first_sum, second_sum)
-            elif self.sum == '*':
-                first_sum = self.a1 * self.b1
-                second_sum = self.a2 * self.b2
-                return '({},{})'.format(first_sum, second_sum)
-            elif self.sum == '/':
-                first_sum = self.a1 // self.b1
-                second_sum = self.a2 // self.b2
-                return '({},{})'.format(first_sum, second_sum)
+
+    elif operation == '-':
+        def calc(a1,a2,b1,b2,b3 = None, a3 = None):
+            c1 = a1 + b1
+            c2 = a2 + b2
+            c3 = a3 + b3
+            return
+
+
+    elif operation == '*':
+        def calc(a1,a2,b1,b2,b3 = None, a3 = None):
+            c1 = a1 + b1
+            c2 = a2 + b2
+            c3 = a3 + b3
+            return
+
+    elif operation == '/':
+        def calc(a1,a2,b1,b2,b3 = None, a3 = None):
+            c1 = a1 + b1
+            c2 = a2 + b2
+            c3 = a3 + b3
+            return
     else:
-        def vector_calc_v3(self):
-            if self.sum == '+':
-                first_sum = self.a1 + self.b1
-                second_sum = self.a2 + self.b2
-                third_sum = self.a3 + self.b3
-                return '({},{},{})'.format(first_sum, second_sum, third_sum)  # добавить вторую функцию для 3х векторов
-            elif self.sum == '-':
-                first_sum = self.a1 - self.b1
-                second_sum = self.a2 - self.b2
-                third_sum = self.a3 - self.b3
-                return '({},{},{})'.format(first_sum, second_sum, third_sum)
-            elif self.sum == '*':
-                first_sum = self.a1 * self.b1
-                second_sum = self.a2 * self.b2
-                third_sum = self.a3 * self.b3
-                return '({},{},{})'.format(first_sum, second_sum, third_sum)
-            elif self.sum == '/':
-                first_sum = self.a1 // self.b1
-                second_sum = self.a2 // self.b2
-                third_sum = self.a3 // self.b3
-                return '({},{},{})'.format(first_sum, second_sum, third_sum)
+        raise Exception('Я могу только делить, умножать, вычитать и прибавлять')
+    return calc
+
+
+def Fackinator(*args):
+    if len(*args) == 4:
+        a1 = args[0]
+        a2 = args[1]
+        b1 = args[2]
+        b2 = args[3]
+        return a1, a2, b1, b2
+    elif len(*args) == 6:
+        a1 = args[0]
+        a2 = args[1]
+        a3 = args[2]
+        b1 = args[3]
+        b2 = args[4]
+        b3 = args[5]
+        return a1, a2, a3, b1, b2, b3
+
+def operate(op, numbers):
+    oper = Calc(op)
+    print(oper(Fackinator(numbers)))
+
+
+
+operate('+', [22,75,88,11])
+
+
